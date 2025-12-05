72d6314d17e87767316d73d15f53d563f96330df
patrick <patrickmastorga@gmail.com>
2025-12-06 01:22:50 +0900

    seperare EMA and refresh_dead

--- VQ/model_EMA.py
+++ VQ/model_EMA.py
@@ -1,95 +1,3 @@
-import torch
-import torch.nn as nn
-
-# model hyperparemters
-LATENT_W = 16
-LATENT_H = 16
-IMG_W = LATENT_W * 4
-IMG_H = LATENT_H * 4
-
-EMBEDDING_DIM = 64
-NUM_EMBEDDINGS = 512
-HIDDEN_CHANNELS = 256
-
-class ResidualBlock(nn.Module):
-    """
-    implementation of the residual block as described in section 4.1 of the original VQ-VAE paper\\
-    ReLU -> 3x3 conv -> ReLU -> 1x1 conv -> skip connection
-    """
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.ReLU(),
-            nn.Conv2d(HIDDEN_CHANNELS, HIDDEN_CHANNELS, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(HIDDEN_CHANNELS, HIDDEN_CHANNELS, kernel_size=1)
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x + self.network(x)
-
-class Encoder(nn.Module):
-    """
-    maps an (3, IMG_H, IMG_W) image tensor to a (EMBEDDING_DIM, LATENT_H, LATENT_W) latent tensor\\
-    downsample -> residual block -> downsample -> residual block -> downsample -> residual block -> 1x1 conv
-    """
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            # (3, IMG_H, IMG_W) image
-            nn.Conv2d(in_channels=3, out_channels=HIDDEN_CHANNELS, kernel_size=1),
-            # (HIDDEN_CHANNELS, IMG_H, IMG_W) hidden
-            ResidualBlock(),
-            nn.Conv2d(in_channels=HIDDEN_CHANNELS, out_channels=HIDDEN_CHANNELS, kernel_size=4, stride=2, padding=1),
-            # (HIDDEN_CHANNELS, IMG_H/2, IMG_W/2) hidden
-            ResidualBlock(),
-            nn.Conv2d(in_channels=HIDDEN_CHANNELS, out_channels=HIDDEN_CHANNELS, kernel_size=4, stride=2, padding=1),
-            # (HIDDEN_CHANNELS, IMG_H/4, IMG_W/4) = (HIDDEN_CHANNELS, LATENT_H, LATENT_W) hidden
-            ResidualBlock(),
-            nn.Conv2d(in_channels=HIDDEN_CHANNELS, out_channels=EMBEDDING_DIM, kernel_size=1),
-            # (EMBEDDING_DIM, LATENT_H, LATENT_W) latents
-        )
-
-    def forward(self, image: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            image (torch.Tensor): image FloatTensor of shape (B, 3, IMG_H, IMG_W) in range [0, 1]
-        Returns:
-            z_e (torch.Tensor): encoder output FloatTensor of shape (B, EMBEDDING_DIM, LATENT_H, LATENT_W)
-        """
-        return self.network(image)
-
-class Decoder(nn.Module):
-    """
-    maps a (EMBEDDING_DIM, LATENT_H, LATENT_W) quantized latent tensor to an (3, IMG_H, IMG_W) image tensor (scaled to [0, 1])\\
-    1x1 conv -> residual block -> upsample -> residual block -> upsample -> residual block -> upsample
-    """
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            # (EMBEDDING_DIM, LATENT_H, LATENT_W) latents
-            nn.Conv2d(in_channels=EMBEDDING_DIM, out_channels=HIDDEN_CHANNELS, kernel_size=1),
-            # (HIDDEN_CHANNELS, LATENT_H, LATENT_W) = (HIDDEN_CHANNELS, IMG_H/4, IMG_W/4) hidden
-            ResidualBlock(),
-            nn.ConvTranspose2d(in_channels=HIDDEN_CHANNELS, out_channels=HIDDEN_CHANNELS, kernel_size=4, stride=2, padding=1),
-            # (HIDDEN_CHANNELS, IMG_H/2, IMG_W/2) hidden
-            ResidualBlock(),
-            nn.ConvTranspose2d(in_channels=HIDDEN_CHANNELS, out_channels=HIDDEN_CHANNELS, kernel_size=4, stride=2, padding=1),
-            # (HIDDEN_CHANNELS, IMG_H, IMG_W) hidden
-            ResidualBlock(),
-            nn.Conv2d(in_channels=HIDDEN_CHANNELS, out_channels=3, kernel_size=1),
-            nn.Sigmoid()
-            # (3, IMG_H, IMG_W) image
-        )
-
-    def forward(self, z_q: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            z_q (torch.Tensor): quantized latent FloatTensor of shape (B, EMBEDDING_DIM, LATENT_H, LATENT_W)
-        Returns:
-            image (torch.Tensor): image FloatTensor of shape (B, 3, IMG_H, IMG_W) in range [0, 1]
-        """
-        return self.network(z_q)
 
 class Quantizer(nn.Module):
     """
@@ -97,7 +5,7 @@ class Quantizer(nn.Module):
     if use_EMA=True, embeddings are learnt automatically as exponential moving averages of the encoder outputs assigned to them over minibatches (see Appendix A.1 of the original VQ-VAE paper)\\
     otherwise, the embeddings are parameters to be learnt with gradient descent on the codebook loss (see section 3.2 of the original VQ-VAE paper)
     """
-    def __init__(self, batch_size=128, decay=0.99):
+    def __init__(self, use_EMA=False, batch_size=0, decay=0.99):
         """
         Args:
             use_EMA (bool): if True, use EMA updates to learn the codebook during training
@@ -105,14 +13,20 @@ class Quantizer(nn.Module):
             decay (float): EMA decay parameter
         """
         super().__init__()
+        self.use_EMA = use_EMA
         self.decay = decay
 
-        # Running cluster counts
-        self.expected_count = batch_size * LATENT_W * LATENT_H / NUM_EMBEDDINGS
-        self.register_buffer('N', torch.full((NUM_EMBEDDINGS,), self.expected_count))
+        # EMA running cluster counts
+        expected_count = batch_size * LATENT_W * LATENT_H / NUM_EMBEDDINGS
+        self.register_buffer('N', torch.full((NUM_EMBEDDINGS,), expected_count))
 
         # codebook dictionary
-        self.register_parameter('e', nn.Parameter(torch.randn(NUM_EMBEDDINGS, EMBEDDING_DIM)))
+        if not self.use_EMA:
+            self.register_parameter('e', nn.Parameter(torch.randn(NUM_EMBEDDINGS, EMBEDDING_DIM)))
+        else:
+            self.register_buffer('e', torch.randn(NUM_EMBEDDINGS, EMBEDDING_DIM))
+            # EMA running cluster sums
+            self.register_buffer('m', self.e.clone() * expected_count)
 
     def nearest_neighbor_indices(self, z_e: torch.Tensor) -> torch.Tensor:
         """
@@ -129,8 +43,8 @@ class Quantizer(nn.Module):
         with torch.no_grad():
             dist = (
                 z_e_flat.pow(2).sum(dim=1, keepdim=True) # ||z||^2 (BHW, 1)
-                + self.e.pow(2).sum(dim=1).unsqueeze(0)  # ||e||^2 (1, NUM_EMBEDDING) # type: ignore
-                - 2 * z_e_flat @ self.e.T                # -2z*e   (BHW, NUM_EMBEDDING) # type: ignore
+                + self.e.pow(2).sum(dim=1).unsqueeze(0)  # ||e||^2 (1, NUM_EMBEDDING)
+                - 2 * z_e_flat @ self.e.T                # -2z*e   (BHW, NUM_EMBEDDING)
             )
         indices_flat = dist.argmin(1)                                   # (BHW,)
         return indices_flat.view(B, H, W).permute(0, 1, 2).contiguous() # (B, H, W)
@@ -142,7 +56,7 @@ class Quantizer(nn.Module):
         Returns:
             z_q (torch.Tensor): quantized latent FloatTensor of shape (B, EMBEDDING_DIM, LATENT_H, LATENT_W)
         """
-        x = nn.functional.embedding(x, self.e)    # (B, H, W, EMBEDDING_DIM) # type: ignore
+        x = nn.functional.embedding(x, self.e)    # (B, H, W, EMBEDDING_DIM)
         return x.permute(0, 3, 1, 2).contiguous() # (B, EMBEDDING_DIM, H, W)
 
     def forward(self, z_e: torch.Tensor, refresh_dead: bool = False) -> torch.Tensor:
@@ -153,11 +67,6 @@ class Quantizer(nn.Module):
         Returns:
             z_q (torch.Tensor): quantized latent FloatTensor of shape (B, EMBEDDING_DIM, LATENT_H, LATENT_W)
         """
-
-        # flatten the embeddings along batch size, height, and width (B, embedding_dim, H, W) -> (BHW, embedding_dim)
-        B, _, H, W = z_e.shape
-        z_e_flat = z_e.permute(0, 2, 3, 1).reshape(-1, EMBEDDING_DIM)
-
         # Dead codebook refresh
         if self.refresh_dead:
             p = self.N / self.N.sum() * NUM_EMBEDDINGS
@@ -167,25 +76,39 @@ class Quantizer(nn.Module):
 
             if len(dead_idx) > 0:
                 choice = torch.randint(0, z_e_flat.shape[0], (len(dead_idx),), device=z_e_flat.device)
-                self.e[dead_idx] = z_e_flat[choice] # type: ignore
-                self.N[dead_idx] = self.expected_count
+                self.e[dead_idx] = z_e_flat[choice]
+
+        # flatten the embeddings along batch size, height, and width (B, embedding_dim, H, W) -> (BHW, embedding_dim)
+        B, _, H, W = z_e.shape
+        z_e_flat = z_e.permute(0, 2, 3, 1).reshape(-1, EMBEDDING_DIM)
 
         # to calculate pairwise distance, use ||z - e||^2 = ||z||^2 - 2z*e + ||e||^2
         with torch.no_grad():
             dist = (
                 z_e_flat.pow(2).sum(dim=1, keepdim=True) # ||z||^2 (BHW, 1)
-                + self.e.pow(2).sum(dim=1).unsqueeze(0)  # ||e||^2 (1, NUM_EMBEDDING) # type: ignore
-                - 2 * z_e_flat @ self.e.T                # -2z*e   (BHW, NUM_EMBEDDING) # type: ignore
+                + self.e.pow(2).sum(dim=1).unsqueeze(0)  # ||e||^2 (1, NUM_EMBEDDING)
+                - 2 * z_e_flat @ self.e.T                # -2z*e   (BHW, NUM_EMBEDDING)
             )
         indices_flat = dist.argmin(1)
 
-        z_q = nn.functional.embedding(indices_flat, self.e).view(B, H, W, EMBEDDING_DIM) # (B, H, W, embedding_dim) # type: ignore
+        z_q = nn.functional.embedding(indices_flat, self.e).view(B, H, W, EMBEDDING_DIM) # (B, H, W, embedding_dim)
         z_q = z_q.permute(0, 3, 1, 2).contiguous()                                       # (B, embedding_dim, H, W)
 
-        # update current minibatch cluster counts
         if self.training:
+            # current minibatch cluster counts
             n_i = torch.bincount(indices_flat, minlength=NUM_EMBEDDINGS).float()
             self.N = self.decay * self.N + (1 - self.decay) * n_i
+
+            # EMA codebook update
+            if self.use_EMA:
+                with torch.no_grad():
+                    # current minibatch cluster sums
+                    m_i = torch.zeros_like(self.e)
+                    m_i.index_add_(0, indices_flat, z_e_flat)
+                    self.m = self.decay * self.m + (1 - self.decay) * m_i
+
+                    # EMA updates
+                    self.e = self.m / (self.N.unsqueeze(1) + 1e-8)
 
         return z_q
 
@@ -193,11 +116,13 @@ class VQ_VAE(nn.Module):
     """
         implements the encoder, decoder, and quantizer into a single model for training
     """
-    def __init__(self, encoder: Encoder, decoder: Decoder, quantizer: Quantizer):
+    def __init__(self, encoder: Encoder, decoder: Decoder, quantizer: Quantizer, use_EMA=False, beta=0.25):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.quantizer = quantizer
+        self.use_EMA = use_EMA
+        self.beta = beta
 
     @torch.no_grad()
     def compute_indices(self, image: torch.Tensor) -> torch.Tensor:
@@ -240,7 +165,7 @@ class VQ_VAE(nn.Module):
         Args:
             input (torch.Tensor): image FloatTensor of shape (B, 3, IMG_H, IMG_W) in range [0, 1]
         Returns:
-            losses (tuple[torch.Tensor, torch.Tensor, torch.Tensor]): reconstruction_loss, commitment_loss, codebook_loss
+            losses (tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]): reconstruction_loss, commitment_loss, codebook_loss; if use_EMA=True, codebook_loss is None
         """
         B = input.shape[0]
         z_e = self.encoder(input)
@@ -253,5 +178,8 @@ class VQ_VAE(nn.Module):
         # compute loss
         reconstruction_loss = nn.functional.mse_loss(reconstructed, input, reduction='sum') / B
         commitment_loss = nn.functional.mse_loss(z_e, z_q.detach())
-        codebook_loss = nn.functional.mse_loss(z_e.detach(), z_q)
-        return reconstruction_loss, commitment_loss, codebook_loss
+        if self.use_EMA:
+            return reconstruction_loss, commitment_loss, None
+        else:
+            codebook_loss = nn.functional.mse_loss(z_e.detach(), z_q)
+            return reconstruction_loss, commitment_loss, codebook_loss
